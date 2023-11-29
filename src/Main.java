23e0cba18d4c01cb1065e796bf43c94bd6d42daa
mattiAR <matti.a.r@outlook.com>
2023-11-29 19:44:31 +0100

    ejerciciosPresentacion

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 
-public class Main {
+public class EjerciciosArrays {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
